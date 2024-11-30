1419b722842e859cf38c105f896c2ddc47785caf
navid bahrami <navidbahrami1996@gmail.com>
2024-11-30 18:17:12 +0330

    index

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ app.set('veiw engine' , 'ejs');
 const server = http.createServer(app);
 const PORT = 4000;
 const io = socketIO(server , {cors : {origin : '*'}});
-
-
+io.on('connection' , socket => {
+    const redisMulti = redisClient.multi()
+    socket.on('message' , ({username , message}) => {
+        redisMulti.rPush('messages' , `${username}:${message}`)
+        io.emit('message' , {username , message})
+    })
+})
+app.get('/' , (req , res) => {
+    res.render('login.ejs')
+})
+app.get('/chat' , (req , res) =>{
+    const {username} = req.query;
+    res.render('chat.ejs' , {username})
+})
 app.listen(PORT , () => {
-    console.log(`SERVER RUN ON ${PORT}`);
+    console.log(`SERVER RUN ON http://localhost:${PORT}`);
 })
